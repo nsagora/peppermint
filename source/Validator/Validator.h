e718cbc34f43f782c76b81cc1dd1ca54ffdeb0a3
Alex Cristea <alex@thinslices.com>
2016-08-06 06:15:30 +0300

    Define the protocol for the ValidationRule

---
+++ source/Validator/Validator.h
@@ -0,0 +1,19 @@
+//
+//  Validator.h
+//  Validator
+//
+//  Created by Alex Cristea on 05/08/16.
+//  Copyright © 2016 NSAgora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Validator.
+FOUNDATION_EXPORT double ValidatorVersionNumber;
+
+//! Project version string for Validator.
+FOUNDATION_EXPORT const unsigned char ValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Validator/PublicHeader.h>
+
+
