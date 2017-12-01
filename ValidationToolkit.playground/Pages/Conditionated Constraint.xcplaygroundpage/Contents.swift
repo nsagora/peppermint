fe12b24b13abfda6b7caac29dc3c163f285d1a07
Alex Cristea <alex@thinslices.com>
2017-12-01 10:43:44 +0200

    Add example of conditionated constraint

---
+++ ValidationToolkit.playground/Pages/Conditionated Constraint.xcplaygroundpage/Contents.swift
@@ -0,0 +1,32 @@
+//: [Previous](@previous)
+
+import Foundation
+import ValidationToolkit
+
+/*:
+ ## Conditionated Constraint
+ 
+ In the following example we use `BlockPredicate` based `Constraint` to evaluate if a username has at least 5 characters. The constraint is conditioned by a `RegexPredicate` based `Constraints` that requires the username to contain only digits.
+ 
+ If the condition doen't take place, then the evaluation fails before checking the lenght.
+ */
+
+let text = "#1234567890"
+let minLenght = BlockPredicate<String> { $0.count >= 5 }
+let isNumerical = RegexPredicate(expression: "^(0|[1-9][0-9]*)$")
+
+var lenghtConstraint = Constraint(predicate: minLenght, error: Form.Username.invalid("Too short."))
+let numericConstraint = Constraint(predicate: isNumerical, error: Form.Username.invalid("Not numeric"))
+lenghtConstraint.add(condition: numericConstraint)
+
+let result = lenghtConstraint.evaluate(with: text)
+
+switch result {
+case .valid:
+    print("You 🤘! ")
+case .invalid(let summary):
+    print(summary.errors)
+}
+
+//: [Next](@next)
+
