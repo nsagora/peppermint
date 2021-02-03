cd0de7248e574e2b03565353d8ce53a83f80d3c4
Alex Cristea <alexcristea@me.com>
2021-02-03 09:34:45 +0200

    Add examples playground

---
+++ ValidationToolkit.playground/Pages/Compound Contraint.xcplaygroundpage/Contents.swift
@@ -0,0 +1,34 @@
+//: [Previous](@previous)
+
+import Foundation
+import ValidationToolkit
+
+/*:
+ ## ConstraintSet
+ 
+ In the following example we use a `ConstraintSet` to evaluate the strength of the user password.
+ */
+
+let lowerCasePredicate = RegexPredicate(expression: "^(?=.*[a-z]).*$")
+let upperCasePredicate = RegexPredicate(expression: "^(?=.*[A-Z]).*$")
+let digitsPredicate = RegexPredicate(expression: "^(?=.*[0-9]).*$")
+let specialChars = RegexPredicate(expression: "^(?=.*[!@#\\$%\\^&\\*]).*$")
+let minLenght = RegexPredicate(expression: "^.{8,}$")
+
+var passwordConstraint = CompoundContraint<String>(allOf:
+    PredicateConstraint(predicate: lowerCasePredicate, error: Form.Password.missingLowercase),
+    PredicateConstraint(predicate: upperCasePredicate, error: Form.Password.missingUpercase),
+    PredicateConstraint(predicate: digitsPredicate, error: Form.Password.missingDigits),
+    PredicateConstraint(predicate: specialChars, error: Form.Password.missingSpecialChars),
+    PredicateConstraint(predicate: minLenght, error: Form.Password.minLenght(8))
+)
+
+let password = "3nGuard!"
+let result = passwordConstraint.evaluate(with: password)
+
+switch result {
+case .success:
+    print("Wow, that's a 💪 password!")
+case .failure(let summary):
+    print(summary.errors.map({$0.localizedDescription}))
+}
---
+++ ValidationToolkit.playground/Pages/Custom Predicate.xcplaygroundpage/Contents.swift
@@ -0,0 +1,38 @@
+//: [Previous](@previous)
+
+import Foundation
+import ValidationToolkit
+
+/*:
+ ## Custom Predicate
+ 
+ In the following example we define a custom predicate which evaluates if the length of the user input is equal to 5 characters.
+ */
+
+public class MinLenghtPredicate: Predicate {
+    
+    public typealias InputType = String
+    
+    private let minLenght:Int
+    
+    public init(minLenght:Int) {
+        self.minLenght = minLenght
+    }
+    
+    public func evaluate(with input: String) -> Bool {
+        return input.count >= minLenght
+    }
+}
+
+let input = "alphabet"
+let predicate = MinLenghtPredicate(minLenght: 5)
+let isValid = predicate.evaluate(with: input)
+
+if isValid {
+    print("That's what I call a long ⛓ of characters!")
+}
+else {
+    print("Expecting at least 5 digits.")
+}
+
+//: [Next](@next)
