37ba52ed63f3f8f288e23eda43de4308ce6c559b
Alex Cristea <alexcristea@me.com>
2021-03-17 17:34:40 +0200

    Add mode for CharacterSetPredicate

--- Sources/Predicates/Standard/CharacterSetPredicate.swift
+++ Sources/Predicates/Standard/CharacterSetPredicate.swift
@@ -5,17 +5,29 @@ import Foundation
  */
 public struct CharacterSetPredicate: Predicate {
 
+    /// The `Mode` enum is used to set the evaluation mode of the input's character set against the character set provided at initialisation.
+    public enum Mode {
+        /// The input's character set should be a subset of the  character set provided at initialisation.
+        case strict
+        
+        /// The input's character set should intersect with the  character set provided at initialisation.
+        case loose
+    }
+    
     public typealias InputType = String
     
     private let characterSet: CharacterSet
+    private let mode: Mode
     
     /**
      Creates and returns a new `CharacterSetPredicate` instance.
      
      - parameter characterSet: A `CharacterSet` used to evaluate a given `String` input.
+     - parameter mode: A `Mode` that describes how the input's character set should be evaluated against the character set provided at initialisation.
      */
-    public init(_ characterSet: CharacterSet) {
+    public init(_ characterSet: CharacterSet, mode: Mode = .strict) {
         self.characterSet = characterSet
+        self.mode = mode
     }
 
     /**
@@ -25,6 +37,13 @@ public struct CharacterSetPredicate: Predicate {
      - returns: `true` if input  contains only characters in the charecter set, otherwise `false`.
      */
     public func evaluate(with input: String) -> Bool {
-        return CharacterSet(charactersIn: input).isStrictSubset(of: characterSet)
+        let inputCharacterSet = CharacterSet(charactersIn: input)
+        switch mode {
+        case .strict:
+            return inputCharacterSet.isSubset(of: characterSet)
+        case .loose:
+            return !inputCharacterSet.intersection(characterSet).isEmpty
+        }
+        
     }
 }
