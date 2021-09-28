141179e4b7685f14109a4a54f7c709ed4e279154
Alex Cristea <alexcristea@me.com>
2021-09-28 22:20:00 +0300

    Refactor URLPredicate tests and fix typo

--- Tests/Predicates/Standard/URLPredicateTests.swift
+++ Tests/Predicates/Standard/URLPredicateTests.swift
@@ -3,25 +3,17 @@ import Peppermint
 
 class URLPredicateTests: XCTestCase {
 
-    var predicate: URLPredicate!
-
-    override func setUp() {
-        super.setUp()
-        predicate = URLPredicate()
-    }
-    
-    override func tearDown() {
-        predicate = nil
-        super.tearDown()
-    }
-
-    func testThatItEvaluatesValuidURLToTrue() {
-        let result = predicate.evaluate(with: "http://www.url.com")
+    func testThatItEvaluatesValidURLToTrue() {
+        let sut = URLPredicate()
+        let result = sut.evaluate(with: "http://www.url.com")
+        
         XCTAssertTrue(result)
     }
 
-    func testThatItEvaluatesInvaluidURLToFalse() {
-        let result = predicate.evaluate(with: "http:\\www.url.com")
+    func testThatItEvaluatesInvalidURLToFalse() {
+        let sut = URLPredicate()
+        let result = sut.evaluate(with: "http:\\www.url.com")
+        
         XCTAssertFalse(result)
     }
 }
