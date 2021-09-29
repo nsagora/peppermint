6428875015609571d325c8b67d10f4d72fe5432c
Alex Cristea <alexcristea@me.com>
2021-09-29 08:31:32 +0300

    Refactor BlockPredicate tests

--- Tests/Predicates/Standard/BlockPredicateTests.swift
+++ Tests/Predicates/Standard/BlockPredicateTests.swift
@@ -3,25 +3,17 @@ import Peppermint
 
 class BlockPredicateTests: XCTestCase {
 
-    var predicate: BlockPredicate<String>!
-
-    override func setUp() {
-        super.setUp()
-        predicate = BlockPredicate { $0 == "valid" }
-    }
-
-    override func tearDown() {
-        predicate = nil
-        super.tearDown()
-    }
-
     func testEvaluateShouldReturnTrueWhenInputIsValid() {
-        let result = predicate.evaluate(with: "valid")
+        let sut = BlockPredicate { $0 == "valid" }
+        let result = sut.evaluate(with: "valid")
+        
         XCTAssertTrue(result)
     }
 
     func testEvaluateShouldReturnFalseWhenInputIsInvalid() {
-        let result = predicate.evaluate(with: "invalid")
+        let sut = BlockPredicate { $0 == "valid" }
+        let result = sut.evaluate(with: "invalid")
+        
         XCTAssertFalse(result)
     }
 }
