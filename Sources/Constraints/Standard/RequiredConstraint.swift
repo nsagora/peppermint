5d7d5d9e21e6d1e27e01841239048758fce14f02
Alex Cristea <alexcristea@me.com>
2021-10-17 22:06:58 +0300

    Add RequiredConstraint documentation

--- Sources/Constraints/Standard/RequiredConstraint.swift
+++ Sources/Constraints/Standard/RequiredConstraint.swift
@@ -1,23 +1,94 @@
 import Foundation
 
+/**
+ A `Constraint` that  check whether the input collection is empty.
+ 
+ ```swift
+ enum Failure: Error {
+     case required
+     case invalidEmail
+ }
+ ```
+ 
+ ```swift
+ let constraint = RequiredConstraint<String, Failure>(error: .required)
+ let result = constraint.evaluate(with: "hello@nsagora.com")
+ ```
+ */
 public struct RequiredConstraint<T: Collection, E: Error>: Constraint {
     
     private let predicate = RequiredPredicate<T>()
     private let errorBuilder: (T) -> E
-    
+  
+    /**
+     Returns a new `RequiredConstraint` instance.
+     
+     ```swift
+     enum Failure: Error {
+         case required
+         case invalidEmail
+     }
+     ```
+     
+     ```swift
+     let constraint = RequiredConstraint<String, Failure>(error: .required)
+     let result = constraint.evaluate(with: "hello@nsagora.com")
+     ```
+     
+     - parameter error: An `Error` that describes why the evaluation has failed.
+     */
     public init(error: E) {
         self.errorBuilder = { _ in return error }
     }
    
+    /**
+     Returns a new `RequiredConstraint` instance.
+     
+     ```swift
+     enum Failure: Error {
+         case required
+         case invalidEmail
+     }
+     ```
+     
+     ```swift
+     let constraint = RequiredConstraint<String, Failure> { _ in .required }
+     let result = constraint.evaluate(with: "hello@nsagora.com")
+     ```
+     
+     - parameter error: A generic closure that dynamically builds an `Error` to describe why the evaluation has failed.
+     */
     public init(errorBuilder: @escaping (T) -> E) {
         self.errorBuilder = errorBuilder
     }
     
+    /**
+     Returns a new `RequiredConstraint` instance.
+     
+     ```swift
+     enum Failure: Error {
+         case required
+         case invalidEmail
+     }
+     ```
+     
+     ```swift
+     let constraint = RequiredConstraint<String, Failure> { .required }
+     let result = constraint.evaluate(with: "hello@nsagora.com")
+     ```
+     
+     - parameter error: A generic closure that dynamically builds an `Error` to describe why the evaluation has failed.
+     */
     public init(errorBuilder: @escaping () -> E) {
         self.errorBuilder = { _ in return errorBuilder() }
     }
     
-    
+    /**
+     Evaluates whether the input collection is empty or not.
+     
+     - parameter input: The input collection to be validated.
+     - returns: `.success` if the input is valid,`.failure` containing the `Summary` with the provided `Error`.
+     */
     public func evaluate(with input: T) -> Result<Void, Summary<E>> {
         let result = predicate.evaluate(with: input)
         if result {
