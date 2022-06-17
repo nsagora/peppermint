6160ff198290debb4770b897068c6b1ff9164bf0
Alex Cristea <alexcristea@me.com>
2022-06-17 08:13:40 +0300

    Refactor the ConstraintBuilder resultBuilder

--- Sources/Constraints/ConstraintBuilder.swift
+++ Sources/Constraints/ConstraintBuilder.swift
@@ -2,59 +2,38 @@ import Foundation
 
 @resultBuilder
 public struct ConstraintBuilder<T, E: Error> {
-    
-    public static func buildExpression<C: Constraint>(_ expression: C) -> ConstraintContainer<T, E> where C.InputType == T, C.ErrorType == E {
-        return ConstraintContainer(expression)
-    }
-    
-    public static func buildEither(first component: ConstraintContainer<T, E>) -> ConstraintContainer<T, E> {
-        return component
-    }
-    
-    public static func buildEither(second component: ConstraintContainer<T, E>) -> ConstraintContainer<T, E> {
-        return component
-    }
-    
-    public static func buildOptional(_ component: ConstraintContainer<T, E>?) -> ConstraintContainer<T, E> {
-        return component ?? ConstraintContainer()
-    }
-    
-    public static func buildLimitedAvailability(_ component: ConstraintContainer<T, E>) -> ConstraintContainer<T, E> {
-        return component
-    }
-    
-    public static func buildBlock(_ components: ConstraintContainer<T, E>...) -> ConstraintContainer<T, E> {
-        return ConstraintContainer.flatMap(containers: components)
-    }
-    
-    public static func buildArray(_ components: [ConstraintContainer<T, E>]) -> ConstraintContainer<T, E> {
-        return ConstraintContainer.flatMap(containers: components)
-    }
-    
-    public static func buildFinalResult(_ component: ConstraintContainer<T, E>) -> [AnyConstraint<T, E>] {
-        return component.constraints
-    }
-}
 
-extension ConstraintBuilder {
+    public typealias Component = [AnyConstraint<T, E>]
     
-    public struct ConstraintContainer<T, E: Error> {
-        
-        private(set) var constraints = [AnyConstraint<T, E>]()
-        
-        init(){ }
-        
-        init<C: Constraint>(_ constraints: [C]) where C.InputType == T, C.ErrorType == E {
-            self.constraints = constraints.map { $0.erase() }
-        }
-        
-        init<C: Constraint>(_ constraints: C...) where C.InputType == T, C.ErrorType == E {
-            self.constraints = constraints.map { $0.erase() }
-        }
-        
-        static func flatMap(containers:  [ConstraintContainer] = []) -> ConstraintContainer {
-            let constraints = containers.reduce([]) { $0 + $1.constraints }
-            return ConstraintContainer(constraints)
-        }
+    public static func buildExpression(_ expression: some Constraint<T, E>) -> Component {
+        return [expression.erase()]
+    }
+    
+    public static func buildEither(first component: Component) -> Component {
+        return component
+    }
+    
+    public static func buildEither(second component: Component) -> Component {
+        return component
+    }
+    
+    public static func buildOptional(_ component: Component?) -> Component {
+        return component ?? []
+    }
+    
+    public static func buildLimitedAvailability(_ component: Component) -> Component {
+        return component
+    }
+    
+    public static func buildBlock(_ components: Component...) -> Component {
+        return components.flatMap { $0 }
+    }
+    
+    public static func buildArray(_ components: [Component]) -> Component {
+        return components.flatMap { $0 }
+    }
+    
+    public static func buildFinalResult(_ component: Component) -> Component {
+        return component
     }
 }
