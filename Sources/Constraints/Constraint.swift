9d293975f60d2e67eac61e63594e0c141c717866
Alex Cristea <alexcristea@me.com>
2023-04-07 08:42:42 +0300

    Decouple Constraint from Async Constraint

--- Sources/Constraints/Constraint.swift
+++ Sources/Constraints/Constraint.swift
@@ -3,13 +3,13 @@ import Foundation
 /**
  The `Constraint` protocol is used to define the structure that must be implemented by concrete constraints.
  */
-public protocol Constraint<InputType, ErrorType>: AsyncConstraint {
+public protocol Constraint<InputType, ErrorType> {
     
     /// A type that provides information about what kind of values the constraint can be evaluated with.
     associatedtype InputType
     
     /// An error type that provides information about why the evaluation failed.
-    associatedtype ErrorType
+    associatedtype ErrorType: Error
     
     /**
      Evaluates the input against the receiver.
@@ -44,6 +44,27 @@ public extension Constraint {
         switch result {
         case .success:
             return input
+        case .failure(let summary):
+            throw summary
+        }
+    }
+}
+
+public extension Constraint {
+    
+    /**
+     Asynchronous evaluates the input against the receiver. When the evaluation is successful, it return the `input`, otherwise it throws the `Summary` of the failing `Constraint`.
+     
+     - parameter input: The input to be validated.
+     - Returns:The `input` when the validation is successful.
+     - Throws: The `Summary` of the failing `Constraint`s when the validation fails.
+     */
+    @available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+    func check(_ input: InputType) async throws -> InputType {
+        let result = await evaluate(with: input)
+        switch result {
+        case .success:
+           return input
         case .failure(let summary):
             throw summary
         }
