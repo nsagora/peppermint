a6e539c96398b833bf358eb2c4e5287d4dd45e12
Alex Cristea <alexcristea@me.com>
2023-04-07 09:17:30 +0300

    Increase coverage for AsyncConstraintWrapper

--- Tests/Constraints/AsyncConstraintWrapperTests.swift
+++ Tests/Constraints/AsyncConstraintWrapperTests.swift
@@ -1,5 +1,5 @@
 import XCTest
-import Peppermint
+@testable import Peppermint
 
 final class AsyncConstraintWrapperTests: XCTestCase {
 
@@ -29,6 +29,20 @@ final class AsyncConstraintWrapperTests: XCTestCase {
         do {
             let result = try await sut.check("validInput")
             XCTAssertEqual(result, "validInput")
+        }
+        catch {
+            XCTFail()
+        }
+    }
+    
+    func testCheckAsyncCallsTheCallbackWithAFailureResultWhenTheInputIsInvalid() async throws {
+        
+        let sut = constraint.async()
+        do {
+            let _ = try await sut.check("invalidInput")
+        }
+        catch let error as Summary<FakeError> {
+            XCTAssertEqual(error, Summary<FakeError>(errors: [.Invalid]))
         }
         catch {
             XCTFail()
